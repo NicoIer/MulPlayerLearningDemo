a1816eb1508a2270af7a8abdeb7075ab6c6115b8
NicoIer <2320595857@qq.com>
2023-03-19 14:50:05 +0800

    (icon)

---
+++ Assets/_Scipts/UI/KitchenObjIcon.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace Kitchen.UI
+{
+    public class KitchenObjIcon : MonoBehaviour
+    {
+        [SerializeField]private Image iconImage;
+        [SerializeField]private Image backgroundImage;
+        public KitchenObjEnum objEnum { get; private set; }
+
+        public void SetData(KitchenObjSo dataSo)
+        {
+            iconImage.sprite = dataSo.sprite;
+            objEnum = dataSo.kitchenObjEnum;
+        }
+    }
+}
---
+++ Assets/_Scipts/UI/PlateHoldPanel.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Kitchen.UI
+{
+    public class PlateHoldPanel : MonoBehaviour
+    {
+        public GameObject iconPrefab;
+        private Plate _plate;
+        private readonly List<KitchenObjIcon> _icons = new List<KitchenObjIcon>();
+
+        private void Awake()
+        {
+            _plate = GetComponentInParent<Plate>();
+        }
+
+        private void OnEnable()
+        {
+            _plate.onIngredientAdded += OnIngredientAdded;
+        }
+
+        private void OnDisable()
+        {
+            _plate.onIngredientAdded -= OnIngredientAdded;
+        }
+
+        private void OnIngredientAdded(object sender, KitchenObjEnum e)
+        {
+            var obj = Instantiate(iconPrefab, transform);
+            var icon = obj.GetComponent<KitchenObjIcon>();
+            _icons.Add(icon);
+            //修改Icon图标
+            var dataSo = DataTableManager.Sigleton.GetKitchenObjSo(e);
+            icon.SetData(dataSo);
+        }
+
+        private void OnIngredientRemoved(object sender, KitchenObjEnum e)
+        {
+            //找到第一个对应的Icon 移除掉
+            KitchenObjIcon target = null;
+            foreach (var icon in _icons)
+            {
+                if (icon.objEnum == e)
+                {
+                    target = icon;
+                    break;
+                }
+            }
+
+            if (target == null)
+            {
+                return;
+            }
+
+            _icons.Remove(target);
+            //ToDO 可以优化 不真正的destroy
+            Destroy(target.gameObject);
+        }
+    }
+}
