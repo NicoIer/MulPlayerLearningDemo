f609296b6df4bff16ae8c388f0cc314ed1af91f3
NicoIer <2320595857@qq.com>
2023-03-24 15:31:18 +0800

    框架更新->Container更新

---
+++ Assets/Plugins/Nico/Manager/TableManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Nico.Design;
+using Nico.Util;
+using UnityEngine;
+
+namespace Nico.Manager
+{
+    /// <summary>
+    /// 表管理器
+    /// </summary>
+    public sealed class TableManager : GlobalSingleton<TableManager>, IInitializable
+    {
+        private Dictionary<Type,IMetaDataContainer> _containers = new Dictionary<Type, IMetaDataContainer>();
+        public void Initialize()
+        {
+            // 获取当前程序集中所有IMetaDataContainer的实现类
+            var types = ReflectUtil.GetTypesByInterface<IMetaDataContainer>(AppDomain.CurrentDomain);
+            foreach (var type in types)
+            {
+                Debug.Log(type.Name);
+            }
+            //查询指定文件夹下的所有表格
+        }
+
+        public T1 GetMetaData<T1>(int idx) where T1 : IMetaData
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
